c7e37196e2652cd9c1f13809c01a2a2c978ba1e3
humbertoyusta <humbertoyusta02@gmail.com>
2022-10-02 23:52:11 +0200

    Adding query resolvers

---
+++ app/GraphQL/Queries/Students.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Services\StudentsService;
+
+final class Students
+{
+    private StudentsService $studentsService;
+
+    public function __construct(StudentsService $studentsService)
+    {
+        $this->studentsService = $studentsService;
+    }
+
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        return $this->studentsService->findAll();
+    }
+}
